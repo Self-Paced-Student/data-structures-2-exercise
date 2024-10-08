4d5c1d7737fabcf993149c4702abe57032908a2a
CW Self Paced Student <testselfpaced@gmail.com>
2024-10-08 11:29:27 +0000

    set done

--- scripts/set.js
+++ scripts/set.js
@@ -2,4 +2,25 @@
 
 function Set() {}
 
+// REMOVE-START
+Set.prototype.add = function (value) {
+  return (this[value] = true);
+};
+
+Set.prototype.contains = function (value) {
+  return !!this[value];
+};
+
+Set.prototype.remove = function (value) {
+  return delete this[value];
+};
+
+Set.prototype.intersection = function (set) {
+  const res = [];
+  const keys = Object.keys(set)
+  keys.forEach((key) => this.contains(key) && res.push(key));
+  return res.length ? res : null;
+};
+// REMOVE-END
+
 module.exports = Set;
